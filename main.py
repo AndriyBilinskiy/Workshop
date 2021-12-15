c2a05677b52e9b8cf618bbc760c26a6962c59fa2
marco <marcothemars@gmail.com>
2021-12-15 11:33:36 +0200

    display field + create empty field

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
 """this is our main"""
+import random
+
+
+def create_field():
+    """10x10"""
+
+    ships_unplaced = 10
+    ships = {4: 1, 3 : 2, 2: 3, 1: 4 }
+
+    while ships_unplaced != 0:
+        print(f"You have {ships_unplaced} ships unplaced. Ships left: ")
+        for key in ships.keys():
+            print(f"{key} size : {ships[key]} ship(s) left.")
+        break
+
+    field = [['_' for _ in range(10)] for __ in range(10)]
+
+
+
+
+
+def display_field(field):
+    alp = 'ABCDEFGHIJ'
+    print('  1 2 3 4 5 6 7 8 9 10')
+
+    for i, line in enumerate(field):
+        print(f'{alp[i]} ', end='')
+        print(*line)
+
 
 if __name__ == "__main__":
-    pass
+    create_field()
