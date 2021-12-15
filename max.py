adb35ba849bc90909eda1fc983c5830ef748fe6d
maxmyk <maksym.mykhasiuta@ucu.edu.ua>
2021-12-15 11:38:55 +0200

    changed

--- max.py
+++ max.py
@@ -3,16 +3,19 @@ Print
 """
 
 
-field = [['_' for _ in range(10)] for __ in range(10)]
-
 def print_array(lst):
     alp = 'ABCDEFGHIJ'
     print('  1 2 3 4 5 6 7 8 9 10')
     for i, line in enumerate(lst):
         print(f'{alp[i]} ', end='')
         print(*line)
-print_array(field)
 
 
-
-ship1 = [1,1,1,1]
+if __name__ == '__main__':
+    field = [['▢' for _ in range(10)] for __ in range(10)]
+    field[2][5] = '>'
+    field[2][6] = '>'
+    field[2][3] = '>'
+    field[3][3] = '>'
+    field[4][3] = '>'
+    print_array(field)
